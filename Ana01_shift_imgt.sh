b05eda2113fb1c20ffcd540b431c2fbec66fcda3
Li-Ming Hsu <50681765+tw70455@users.noreply.github.com>
2025-04-27 15:55:17 -0400

    Update Ana01_shift_imgt.sh

--- Ana01_shift_imgt.sh
+++ Ana01_shift_imgt.sh
@@ -5,14 +5,13 @@ set FILE_PATH="/Volumes/ProcessDisk/fMRIData/"
 set NUMS="Subj01 Subj02 Subj03"
 
 
-
 foreach ratNum ($NUMS)
-	# Remove the previous resampled anatomical file if it exists
-	rm "${FILE_PATH}${ratNum}_Anat_RPS.nii.gz"
-	3dresample -inset "${FILE_PATH}${ratNum}_Anat.nii.gz" -prefix "${FILE_PATH}${ratNum}_Anat_RPS.nii.gz"
-	
-	# Remove the previous file that has been resampled to 0.32 mm resolution if it exists
-	rm "${FILE_PATH}${ratNum}_Anat_RPS032.nii.gz"
-	3dresample -dxyz 0.32 0.32 0.32 -inset "${FILE_PATH}${ratNum}_Anat_RPS.nii.gz" -prefix "${FILE_PATH}${ratNum}_Anat_RPS032.nii.gz"
+	if  ( -e "${FILE_PATH}${ratNum}_Anat.nii.gz" ) then
+		# Remove the previous file that has been resampled to 0.32 mm resolution if it exists
+		rm "${FILE_PATH}${ratNum}_Anat_032.nii.gz"
+		3dresample -dxyz 0.32 0.32 0.32 -inset "${FILE_PATH}${ratNum}_Anat.nii.gz" -prefix "${FILE_PATH}${ratNum}_Anat_032.nii.gz"
+	else
+		echo "Warning! ${FILE_PATH}${ratNum}_Anat.nii.gz not found."
+	endif
 end
 
